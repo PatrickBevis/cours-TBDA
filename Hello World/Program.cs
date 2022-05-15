b02cfeb68f3157731fc129a9c82f3d6f322aa4a8
PatrickBevis <bevis.patrick@gmail.com>
2022-05-15 14:52:51 +0200

    découverte C#

---
+++ Hello World/Program.cs
@@ -0,0 +1,11 @@
+Console.WriteLine("Veuillez taper votre nom"); 
+
+string nom = Console.ReadLine();
+
+Console.WriteLine("Veuillez taper votre prenom");
+
+
+string prenom = Console.ReadLine();
+
+Console.WriteLine("Hello"+" " +  nom +" " + prenom);
+
